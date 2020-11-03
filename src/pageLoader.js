fbcfa5b97fe7e20f598468e3c6efeb0553b7bed1
Kirill Bogdanov <kiralbogdanov@gmail.com>
2020-11-03 10:38:24 +0300

    Fixed review comments #1

--- src/pageLoader.js
+++ src/pageLoader.js
@@ -42,92 +42,96 @@ const writeFile = (destination, data) => fs.writeFile(destination, data)
     throw new Error(`Error while writing file ${destination}: ${e.message}`);
   });
 
-const makeLoadResourcesTasks = (resources) => resources.map(
-  ({ url: fileUrl, destination: fileDestination }) => ({
+const modifyHtml = (html, origin, filesDirPath) => {
+  const tagNamesToProcess = Object.keys(tagToLinkAttrNameMapping);
+  const $ = cheerio.load(html, { decodeEntities: false });
+  const filesDirName = path.parse(filesDirPath).base;
+  const resources = [];
+
+  tagNamesToProcess.forEach((tagName) => {
+    const tags = $(tagName);
+    const linkAttrName = tagToLinkAttrNameMapping[tagName];
+
+    tags.each((i, elem) => {
+      const currentLink = $(elem).attr(linkAttrName);
+      const fileUrl = new URL(currentLink, origin);
+
+      if (fileUrl.origin === origin && currentLink) {
+        const fileName = makeFileName(fileUrl);
+        const fileDestination = path.join(filesDirPath, fileName);
+        const fileRelativeDestination = path.join(filesDirName, fileName);
+
+        if (!resources.find((file) => file.url === fileUrl.href)) {
+          resources.push({ url: fileUrl.href, destination: fileDestination });
+        }
+        $(elem).attr(linkAttrName, fileRelativeDestination);
+      }
+    });
+  });
+
+  return {
+    modifiedHtml: $.html(),
+    resources,
+  };
+};
+
+const downloadResources = (resources) => {
+  const tasks = resources.map(({ url: fileUrl, destination: fileDestination }) => ({
     title: fileUrl,
     task: () => requestGet(fileUrl)
       .then(({ data }) => writeFile(fileDestination, data)),
-  }),
-);
+  }));
+
+  const listr = new Listr(tasks, { concurrent: true });
+
+  return listr.run();
+};
 
 const pageLoader = (url, dirPath = process.cwd()) => {
   const pageUrl = new URL(url);
-  const { origin } = pageUrl;
-  const pageFileName = makeFileName(pageUrl);
-  const resultFilePath = path.join(dirPath, pageFileName);
-  const filesDirName = makePageFilesDirName(pageUrl);
-  const filesDirPath = path.join(dirPath, filesDirName);
+  const resultFilePath = path.join(dirPath, makeFileName(pageUrl));
+  const filesDirPath = path.join(dirPath, makePageFilesDirName(pageUrl));
 
   log('Downloading HTML');
   return requestGet(pageUrl.href)
     .then(({ data: html }) => {
       log('Page response received. Modifying HTML');
-      const tagNamesToProcess = Object.keys(tagToLinkAttrNameMapping);
-      const $ = cheerio.load(html, { decodeEntities: false });
-      const resources = [];
-
-      tagNamesToProcess.forEach((tagName) => {
-        const tags = $(tagName);
-        const linkAttrName = tagToLinkAttrNameMapping[tagName];
-
-        tags.each((i, elem) => {
-          const currentLink = $(elem).attr(linkAttrName);
-          const fileUrl = new URL(currentLink, origin);
-
-          if (fileUrl.origin === origin && currentLink) {
-            const fileName = makeFileName(fileUrl);
-            const fileDestination = path.join(filesDirPath, fileName);
-            const fileRelativeDestination = path.join(filesDirName, fileName);
-
-            if (!resources.find((file) => file.url === fileUrl.href)) {
-              resources.push({ url: fileUrl.href, destination: fileDestination });
-            }
-            $(elem).attr(linkAttrName, fileRelativeDestination);
-          }
-        });
-      });
+      const { modifiedHtml, resources } = modifyHtml(html, pageUrl.origin, filesDirPath);
 
       return {
         page: {
-          data: $.html(),
+          data: modifiedHtml,
           destination: resultFilePath,
         },
         resources,
       };
     })
-    .then((data) => {
+    .then(({ page, resources }) => {
       log('HTML modified');
-      const filesCount = data.resources.length;
+      const filesCount = resources.length;
 
       if (filesCount > 0) {
         log(`${filesCount} page resources to download found. Creating files directory`);
 
         return fs.mkdir(filesDirPath)
           .then(() => {
-            log('Files directory created');
-            return data;
+            log('Files directory created. Starting downloading page resources');
+            return downloadResources(resources);
+          })
+          .then(() => {
+            log('Page resources downloaded');
+            return page;
           })
           .catch((e) => {
             throw new Error(`Error while creating files directory ${filesDirPath}: ${e.message}`);
           });
       }
 
-      log('No additional resources found. Skipping files directory creation');
-      return data;
+      log('No additional resources found');
+      return page;
     })
-    .then(({ page, resources }) => {
-      log('Starting downloading page resources');
-      const filesTasks = makeLoadResourcesTasks(resources);
-      const listr = new Listr(filesTasks, { concurrent: true });
-
-      const resourcesPromise = listr.run()
-        .then(() => log('Page resources downloaded'));
-      const pagePromise = writeFile(page.destination, page.data)
-        .then(() => log(`HTML written to ${page.destination}`));
-      const promises = [resourcesPromise, pagePromise];
-
-      return Promise.all(promises);
-    });
+    .then(({ data, destination }) => writeFile(destination, data))
+    .then(() => log(`HTML written to ${resultFilePath}`));
 };
 
 export default pageLoader;
